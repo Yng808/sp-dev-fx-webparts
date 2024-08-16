b91e378146f9c0c256706c52c4e83233880ca7b6
chris.young <chris.young@DEV-2016-CCY>
2024-08-16 07:09:22 +0000

    hide day and week tabs

--- samples/react-rhythm-of-business-calendar/src/components/views/Views.ts
+++ samples/react-rhythm-of-business-calendar/src/components/views/Views.ts
@@ -1,13 +1,13 @@
 import { arrayToMap } from "common";
 import { IViewDescriptor } from "./IViewDescriptor";
-import { DayViewDescriptor } from "./day/DayView";
-import { WeekViewDescriptor } from "./week/WeekView";
+//import { DayViewDescriptor } from "./day/DayView";
+//import { WeekViewDescriptor } from "./week/WeekView";
 import { MonthViewDescriptor } from "./month/MonthView";
 import { QuarterViewDescriptor } from "./quarter/QuarterView";
 
 export const ViewDescriptors: IViewDescriptor[] = [
-    DayViewDescriptor,
-    WeekViewDescriptor,
+    //DayViewDescriptor,
+    //WeekViewDescriptor,
     MonthViewDescriptor,
     QuarterViewDescriptor
 ];
