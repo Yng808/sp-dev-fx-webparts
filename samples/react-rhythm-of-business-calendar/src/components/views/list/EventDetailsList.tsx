82834b4751a513136fee316493e246127d9cac8e
Chris <butugruv@gmail.com>
2024-10-03 00:18:08 -1000

    include description when searching

--- samples/react-rhythm-of-business-calendar/src/components/views/list/EventDetailsList.tsx
+++ samples/react-rhythm-of-business-calendar/src/components/views/list/EventDetailsList.tsx
@@ -1,50 +1,11 @@
 import React, { FC, useEffect, useRef, useState } from 'react';
-import { DetailsList, DetailsListLayoutMode, IColumn, IDetailsListStyles, SelectionMode, Stack, TextField, Sticky, StickyPositionType, IDetailsHeaderProps, ScrollablePane, ScrollbarVisibility } from '@fluentui/react';
 import { EventOccurrence } from 'model';
 import moment from 'moment';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 interface EventDetailsListProps {
     cccurrences: readonly EventOccurrence[];
 }
-
-// Define the custom header renderer with explicit types
-const onRenderDetailsHeader = (props: IDetailsHeaderProps, defaultRender?: (props: IDetailsHeaderProps) => JSX.Element | null) => {
-    if (!props || !defaultRender) {
-        return null;
-    }
-
-    return (
-        <Sticky>
-            {defaultRender(props)}
-        </Sticky>
-    );
-};
-
-// custom table styles
-const detailsListStyles: Partial<IDetailsListStyles> = {
-    headerWrapper: {
-        selectors: {
-            '.ms-DetailsHeader': {
-                backgroundColor: '#0078d4',  // Background color of the header row
-                color: '#ffffff',            // Text color of the header row
-            },
-            '.ms-DetailsHeader-cellTitle': {
-                color: '#ffffff',            // Text color of the header cell
-            },
-            '.ms-DetailsHeader-cell': {
-                selectors: {
-                    ':hover': {
-                        backgroundColor: '#0078d4',  // Background color on hover
-                        color: '#ffffff',            // Text color on hover
-                    }
-                }
-            },
-            '.ms-DetailsHeader-cellTitle:hover': {
-                color: '#ffffff',  // Ensure text color stays white on hover
-            }
-        },
-    },
-};
 
 const EventDetailsList: FC<EventDetailsListProps> = ({ cccurrences }) => {
     const [filteredEvents, setFilteredEvents] = useState<EventOccurrence[]>([...cccurrences]);
@@ -78,6 +39,7 @@ const EventDetailsList: FC<EventDetailsListProps> = ({ cccurrences }) => {
             filtered = filtered.filter(event => {
                 return (
                     event.title.toLowerCase().includes(query) ||
+                    (event.description && event.description.toLowerCase().includes(query)) || 
                     Object.values(event).some(value =>
                         typeof value === 'string' && value.toLowerCase().includes(query)
                     )
@@ -91,171 +53,6 @@ const EventDetailsList: FC<EventDetailsListProps> = ({ cccurrences }) => {
         setFilteredEvents(filtered);
         console.log('filtered events', filteredEvents);
     }, [startDate, endDate, searchQuery, cccurrences]);
-
-    const columns: IColumn[] = [
-        {
-            key: 'column1',
-            name: 'Type',
-            fieldName: 'refinerValues',
-            minWidth: 120,
-            maxWidth: 150,
-            isResizable: true,
-            onRender: (item: EventOccurrence) => {
-                const refinerValues = item.getRefinerValuesForRefinerId(1);
-                return (
-                    <div>
-                        {refinerValues.map((rv, index) => {
-                            const backgroundColor = rv.color.toHexString();
-                            const textColor = rv.color.isDarkColor() ? '#ffffff' : '#000000'; // Use the method from the Color class
-        
-                            return (
-                                <span
-                                    key={index}
-                                    style={{
-                                        backgroundColor: backgroundColor,
-                                        color: textColor,  // Set text color based on the color's luminance
-                                        padding: '2px 4px',
-                                        borderRadius: '3px',
-                                        marginRight: '4px',
-                                        display: 'inline-block'
-                                    }}
-                                >
-                                    {rv.title}
-                                </span>
-                            );
-                        })}
-                    </div>
-                );
-            }
-        },
-        
-        {
-            key: 'column2',
-            name: 'Title',
-            fieldName: 'title',
-            minWidth: 250,
-            maxWidth: 300,
-            isResizable: true,
-            onRender: (item: EventOccurrence) => item.title,
-        },   
-        {
-            key: 'column10',
-            name: 'Decision Brief',
-            fieldName: 'refinerValues',
-            minWidth: 120,
-            maxWidth: 150,
-            isResizable: true,
-            onRender: (item: EventOccurrence) => {
-                const refinerValues = item.getRefinerValuesForRefinerName('Decision Brief');
-                return (
-                    <div>
-                        {refinerValues.map((rv) => (
-                            <div key={rv.title}>{rv.title}</div>
-                        ))}
-                    </div>
-                );
-            }
-        },     
-        {
-            key: 'column3',
-            name: 'Start Date',
-            fieldName: 'startDate',
-            minWidth: 100,
-            maxWidth: 150,
-            isResizable: true,
-            onRender: (item: EventOccurrence) => item.isAllDay ? item.start.format('MM/DD/YYYY') : item.start.format('MM/DD/YYYY HHmm'),
-        },
-        {
-            key: 'column4',
-            name: 'End Date',
-            fieldName: 'endDate',
-            minWidth: 100,
-            maxWidth: 150,
-            isResizable: true,
-            onRender: (item: EventOccurrence) => item.isAllDay ? item.end.format('MM/DD/YYYY') : item.end.format('MM/DD/YYYY HHmm'),
-        },
-        /* {
-            key: 'column4',
-            name: 'COM Decision',
-            fieldName: 'comDecision',
-            minWidth: 100,
-            maxWidth: 150,
-            isResizable: true,
-            onRender: (item: EventOccurrence) => item.comDecision,
-        }, */
-        
-        {
-            key: 'column5',
-            name: 'Read Ahead Due Date',
-            fieldName: 'readAheadDueDate',
-            minWidth: 100,
-            maxWidth: 150,
-            isResizable: true,
-            onRender: (item: EventOccurrence) => item.readAheadDueDate ? item.readAheadDueDate.format('MM/DD/YYYY') : '-',
-        },
-        {
-            key: 'column6',
-            name: 'IPC OPR',
-            fieldName: 'refinerValues',
-            minWidth: 120,
-            maxWidth: 150,
-            isResizable: true,
-            onRender: (item: EventOccurrence) => {
-                const refinerValues = item.getRefinerValuesForRefinerName('IPC OPR');
-                return (
-                    <div>
-                        {refinerValues.map((rv) => (
-                            <div key={rv.title}>{rv.title}</div>
-                        ))}
-                    </div>
-                );
-            }
-        },
-        {
-            key: 'column7',
-            name: 'IPC Attendee',
-            fieldName: 'refinerValues',
-            minWidth: 120,
-            maxWidth: 150,
-            isResizable: true,
-            onRender: (item: EventOccurrence) => {
-                const refinerValues = item.getRefinerValuesForRefinerName('IPC Attendee');
-                return (
-                    <div>
-                        {refinerValues.map((rv) => (
-                            <div key={rv.title}>{rv.title}</div>
-                        ))}
-                    </div>
-                );
-            }
-        },
-        {
-            key: 'column8',
-            name: 'Description',
-            fieldName: 'description',
-            minWidth: 200,
-            maxWidth: 400,
-            isResizable: true,
-            onRender: (item: EventOccurrence) => {
-                return (
-                    <span
-                        style={{
-                            whiteSpace: 'normal',  // Allow text to wrap to the next line
-                            wordWrap: 'break-word',  // Break long words to prevent overflow
-                            overflowWrap: 'break-word',  // Handles overflow in long words
-                        }}
-                    >
-                        {item.description}
-                    </span>
-                );
-            },
-        },
-        
-        
-        // Add more columns as needed for other text-based fields
-    ];
-
-
   
     return (
         <div className="container">
