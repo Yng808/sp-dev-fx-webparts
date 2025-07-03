d55464cc50b244fa2b357e17b1bfc47f95050794
jerson.martinez <jerson.martinez.admin@flankspeed.onmicrosoft.us>
2025-07-03 18:14:21 +0000

    r - unused views

--- samples/react-rhythm-of-business-calendar/src/components/views/Views.ts
+++ samples/react-rhythm-of-business-calendar/src/components/views/Views.ts
@@ -3,17 +3,17 @@ import { IViewDescriptor } from "./IViewDescriptor";
 import { DayViewDescriptor } from "./day/DayView";
 import { WeekViewDescriptor } from "./week/WeekView";
 import { MonthViewDescriptor } from "./month/MonthView";
-import { QuarterViewDescriptor } from "./quarter/QuarterView";
-import { PieChartViewDescriptor } from "./reports/PieChartView";
-import { ListViewDescriptor } from "./list/ListView";
+//import { QuarterViewDescriptor } from "./quarter/QuarterView";
+//import { PieChartViewDescriptor } from "./reports/PieChartView";
+//import { ListViewDescriptor } from "./list/ListView";
 
 export const ViewDescriptors: IViewDescriptor[] = [
     DayViewDescriptor,
     WeekViewDescriptor,
-    MonthViewDescriptor,
-    QuarterViewDescriptor,
-    PieChartViewDescriptor,
-    ListViewDescriptor
+    MonthViewDescriptor
+    //QuarterViewDescriptor,
+    //PieChartViewDescriptor,
+    //ListViewDescriptor
 ];
 
 export const ViewDescriptorsById = arrayToMap(ViewDescriptors, v => v.id);
